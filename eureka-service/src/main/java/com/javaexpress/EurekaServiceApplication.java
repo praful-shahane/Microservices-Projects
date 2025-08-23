6eb68a0d7ad25eccf8578bdca828c6d26469f4ae
praful-shahane <prshahane2@gmail.com>
2025-08-23 20:11:04 +0530

    changes related to eureka service discovery

--- eureka-service/src/main/java/com/javaexpress/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/javaexpress/EurekaServiceApplication.java
@@ -2,14 +2,14 @@ package com.javaexpress;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class ProductServiceApplication {
+@EnableEurekaServer
+public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication.class, args);
+		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
 
 }
