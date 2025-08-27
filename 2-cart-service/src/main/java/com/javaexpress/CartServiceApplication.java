c88a9aecf27455f3c78461a47da3639ce0a5fcb7
praful-shahane <prshahane2@gmail.com>
2025-08-27 17:41:51 +0530

    changes related distributed tracing & microservices lecture day 10

--- 2-cart-service/src/main/java/com/javaexpress/CartServiceApplication.java
+++ 2-cart-service/src/main/java/com/javaexpress/CartServiceApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import feign.micrometer.MicrometerCapability;
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -13,5 +17,15 @@ public class CartServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CartServiceApplication.class, args);
 	}
+	
+	
+	/*
+	 * add one object to register MicrometerCapability
+	 */
+	@Bean
+	MicrometerCapability capability(MeterRegistry registry){
+		   return new MicrometerCapability(registry);
+	   }
+	
 
 }
